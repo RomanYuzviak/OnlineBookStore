c858ea873d599efbcc0d1210f4d070faf8d59c5c
Roman Yuzviak <3romanyuzviak@gmail.com>
2023-10-25 22:58:03 +0300

    Pageable implemented

--- src/main/java/com/example/onlinebookstore/controller/BookController.java
+++ src/main/java/com/example/onlinebookstore/controller/BookController.java
@@ -6,6 +6,7 @@ import com.example.onlinebookstore.service.BookService;
 import jakarta.validation.Valid;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,8 +29,8 @@ public class BookController {
     }
 
     @GetMapping
-    public List<BookDto> getAll() {
-        return bookService.findAll();
+    public List<BookDto> getAll(Pageable pageable) {
+        return bookService.findAll(pageable);
     }
 
     @GetMapping("/{id}")
