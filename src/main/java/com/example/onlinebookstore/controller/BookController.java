68a8c216c6d2e9870a238488d7862758508ded02
Roman Yuzviak <3romanyuzviak@gmail.com>
2023-10-25 23:37:23 +0300

    swagger implemented

--- src/main/java/com/example/onlinebookstore/controller/BookController.java
+++ src/main/java/com/example/onlinebookstore/controller/BookController.java
@@ -3,6 +3,8 @@ package com.example.onlinebookstore.controller;
 import com.example.onlinebookstore.dto.BookDto;
 import com.example.onlinebookstore.dto.CreateBookRequestDto;
 import com.example.onlinebookstore.service.BookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +20,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+//swagger doc http://localhost:8080/swagger-ui/index.html#/
+@Tag(name = "Book CRUD API", description = "Endpoints to manage books")
 @RestController
 @RequestMapping(value = "/api/books")
 public class BookController {
@@ -29,27 +33,32 @@ public class BookController {
     }
 
     @GetMapping
+    @Operation(summary = "get all books")
     public List<BookDto> getAll(Pageable pageable) {
         return bookService.findAll(pageable);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "get a book by id")
     public BookDto findById(@PathVariable Long id) {
         return bookService.get(id);
     }
 
     @PostMapping
+    @Operation(summary = "create a new book")
     public BookDto save(@RequestBody @Valid CreateBookRequestDto requestDto) {
         return bookService.save(requestDto);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "update a book by id")
     BookDto update(@PathVariable Long id, @RequestBody @Valid CreateBookRequestDto requestDto) {
         return bookService.update(id, requestDto);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
+    @Operation(summary = "delete a book by id")
     public void delete(@PathVariable Long id) {
         bookService.delete(id);
     }
