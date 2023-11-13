0e944d80a800eb357a1cf4bcdd323aad4854d7ce
Roman Yuzviak <3romanyuzviak@gmail.com>
2023-11-13 02:19:38 +0200

    category infrastructure set up

--- src/main/java/com/example/onlinebookstore/repository/BookRepository.java
+++ src/main/java/com/example/onlinebookstore/repository/BookRepository.java
@@ -1,8 +1,21 @@
 package com.example.onlinebookstore.repository;
 
 import com.example.onlinebookstore.model.Book;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface BookRepository extends JpaRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
 
+    @EntityGraph(attributePaths = "categories")
+    Optional<Book> findById(Long id);
+
+    @EntityGraph(attributePaths = "categories")
+    Page<Book> findAll(Pageable pageable);
+
+    List<Book> findAllByCategories_Id(Long id, Pageable pageable);
 }
