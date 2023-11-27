a4b934c5d5ec802227607f129c382517e1335a7e
Roman Yuzviak <3romanyuzviak@gmail.com>
2023-11-28 01:29:35 +0200

    deleted extra field

--- src/main/java/com/example/onlinebookstore/OnlineBookStoreApplication.java
+++ src/main/java/com/example/onlinebookstore/OnlineBookStoreApplication.java
@@ -1,13 +1,10 @@
 package com.example.onlinebookstore;
 
-import com.example.onlinebookstore.service.BookService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OnlineBookStoreApplication {
-    private BookService bookService;
-
     public static void main(String[] args) {
         SpringApplication.run(OnlineBookStoreApplication.class, args);
     }
