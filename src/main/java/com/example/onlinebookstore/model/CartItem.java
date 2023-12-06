c1f523d55cbed6fa44587b73e70e3ae761d2c2ca
Roman Yuzviak <3romanyuzviak@gmail.com>
2023-12-06 18:16:59 +0200

    soft delete to cart

--- src/main/java/com/example/onlinebookstore/model/CartItem.java
+++ src/main/java/com/example/onlinebookstore/model/CartItem.java
@@ -12,10 +12,14 @@ import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Entity
 @Table(name = "cart_items")
 @Data
+@SQLDelete(sql = "UPDATE cart_items SET is_deleted = true WHERE id=?")
+@Where(clause = "is_deleted=false")
 public class CartItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -35,4 +39,7 @@ public class CartItem {
 
     @Column(nullable = false)
     private Integer quantity;
+
+    @Column(nullable = false)
+    private boolean isDeleted = false;
 }
--- src/main/java/com/example/onlinebookstore/model/ShoppingCart.java
+++ src/main/java/com/example/onlinebookstore/model/ShoppingCart.java
@@ -1,6 +1,7 @@
 package com.example.onlinebookstore.model;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -15,10 +16,14 @@ import java.util.Set;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Entity
 @Table(name = "shopping_carts")
 @Data
+@SQLDelete(sql = "UPDATE shopping_carts SET is_deleted = true WHERE id=?")
+@Where(clause = "is_deleted=false")
 public class ShoppingCart {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -35,4 +40,7 @@ public class ShoppingCart {
     @OneToMany(mappedBy = "shoppingCart",
             cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<CartItem> cartItems = new HashSet<>();
+
+    @Column(nullable = false)
+    private boolean isDeleted = false;
 }
