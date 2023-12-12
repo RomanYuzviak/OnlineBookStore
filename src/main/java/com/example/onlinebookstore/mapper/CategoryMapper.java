e2701219beaf5b0f5fc9f6687b3fa501bfe323d1
Roman Yuzviak <3romanyuzviak@gmail.com>
2023-12-12 15:19:18 +0200

    a few fixes added

--- src/main/java/com/example/onlinebookstore/mapper/CategoryMapper.java
+++ src/main/java/com/example/onlinebookstore/mapper/CategoryMapper.java
@@ -5,10 +5,13 @@ import com.example.onlinebookstore.dto.category.CategoryDto;
 import com.example.onlinebookstore.dto.category.CreateCategoryRequestDto;
 import com.example.onlinebookstore.model.Category;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper(config = MapperConfig.class)
 public interface CategoryMapper {
     CategoryDto toDto(Category category);
 
     Category toCategory(CreateCategoryRequestDto categoryRequestDto);
+
+    void updateCategory(CreateCategoryRequestDto dto, @MappingTarget Category category);
 }
